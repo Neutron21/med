6f27bf9de6db631cddbc9891e0e1dd9ca9175fd0
MarioMariovictorVictor <mariovictor@MacBook-Air-de-MarioVictor.local>
2024-08-13 21:56:52 -0600

    se crea formilario de paciente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { RegistroComponent } from './registro/registro.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
 import { CajaComponent } from './caja/caja.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { CajaComponent } from './caja/caja.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent implements OnInit {
+  resultQuery: any;
+  formData: any = {
+    email: '',
+    nombre: '',
+    fechaNacimiento: '',
+    doctorId: null,
+    diagnostico: '',
+    observaciones: ''
+  };
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSubmit() {
+    console.log('Formulario enviado', this.formData);
 
+}
+onKeyPress(event: KeyboardEvent) {
+  const inputChar = String.fromCharCode(event.keyCode);
+  const pattern = /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s]*$/;
+  
+  if (!pattern.test(inputChar)) {
+    event.preventDefault();
+  }
+}
 }
