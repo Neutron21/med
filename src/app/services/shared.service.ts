daf8fd69c76a25a566db466dfdc2e7301dfff15f
victorsilvaHR <victor.hugo.silva01@gmail.com>
2024-12-02 10:48:44 -0600

    se agrega observable a shared

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -7,9 +7,7 @@ import { PxService } from './px.service';
   providedIn: 'root'
 })
 export class SharedDataService {
-  constructor (
-   private pxService: PxService,
-  ){}
+  constructor(private pxService: PxService) {}
 
   showSeccion: Secciones = {
     s1: true,
@@ -21,10 +19,10 @@ export class SharedDataService {
     s7: false,
     s8: false, 
     s9: false
-  }
-  idPaciente: number|null = null;
+  };
+  idPaciente: number | null = null;
+  idDoctor: any = {}; 
   verTodo: boolean = false;
-  // cleanHistorial: boolean = false;
 
   private seccionActiva = new Subject<Secciones>();
   seccionObservable = this.seccionActiva.asObservable();
@@ -33,13 +31,24 @@ export class SharedDataService {
     this.seccionActiva.next(newSeccion);
     this.showSeccion = newSeccion;
   }
-  private idPacienteSubject = new Subject<number|null>();
+
+  private idPacienteSubject = new Subject<number | null>();
   idPacienteObservable = this.idPacienteSubject.asObservable();
   
-  cambiarIdPaciente(id: number|null) {
+  cambiarIdPaciente(id: number | null): void {
     this.idPacienteSubject.next(id);
     this.idPaciente = id;
   }
+
+  private idDoctorSubject = new Subject<any>();
+  idDoctorObservable = this.idDoctorSubject.asObservable();
+
+  cambiarIdDoctor(user: any): void {
+    this.idDoctorSubject.next(user);
+    this.idDoctor = user;
+  }
+  
+
   private allSectionsVisible = new Subject<boolean>();
   allSectionsVisibleObs = this.allSectionsVisible.asObservable();
   
@@ -47,111 +56,37 @@ export class SharedDataService {
     this.allSectionsVisible.next(showSeccion);
     this.verTodo = showSeccion;
   }
-  //Observable para limpiar el historial
+
+  // Observable para limpiar el historial
   private resetHistorial = new Subject<boolean>();
   resetHistorialObs = this.resetHistorial.asObservable();
   
   cleanHistorial(showSeccion: boolean) {
     this.resetHistorial.next(showSeccion);
-    // this.cleanHistorial = showSeccion;
   }
+
   seccionesCompletadas(seccion: string | null) {
-    let body = {}
+    let body = {};
     switch (seccion) {
       case 's1':
-       body = JSON.parse(sessionStorage.getItem('s1')+'');
-       if (!!body) {
-        this.pxService.datosGeneralesPost(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });
-      }
-        break;
-      case 's2':
-        body = JSON.parse(sessionStorage.getItem('s2')+'');
+        body = JSON.parse(sessionStorage.getItem('s1') + '');
         if (!!body) {
-        this.pxService.deportivoFm(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });
-      }
-        break;
-      case 's3':
-        body = JSON.parse(sessionStorage.getItem('s3')+'');
-        if (!!body) {
-        this.pxService.medidasFm(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });
-      }
-        break;
-      case 's4':
-        body = JSON.parse(sessionStorage.getItem('s4')+'');
-        if (!!body) {
-        this.pxService.antecedentesFm(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });
-      }
-        break;
-      case 's5':
-        body = JSON.parse(sessionStorage.getItem('s5')+'');
-        if (!!body) {
-          this.pxService.antecedentesPatFm(body).subscribe((response: any) => {
-            console.log("Paciente registrado con éxito, " + response.message);    
-          }, (error: any) =>{
-            console.log("Error al registrar paciente: " + error.error.error);
-          });
+          this.pxService.datosGeneralesPost(body).subscribe(
+            (response: any) => {
+              console.log('Paciente registrado con éxito, ' + response.message);
+            },
+            (error: any) => {
+              console.log('Error al registrar paciente: ' + error.error.error);
+            }
+          );
         }
         break;
-      case 's6':
-        body = JSON.parse(sessionStorage.getItem('s6')+'');
-        if (!!body) {
-        this.pxService.antecedentesNoPatFm(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });
-      }
-        break;
-      case 's7':
-        body = JSON.parse(sessionStorage.getItem('s7')+'');
-        if (!!body) {
-        this.pxService.mujerFm(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });
-      }
-        break;
-      case 's8':
-        body = JSON.parse(sessionStorage.getItem('s8')+'');
-        if (!!body) {
-        this.pxService.pediatricoFm(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });   
-      }     
-        break;
-      case 's9':
-        body = JSON.parse(sessionStorage.getItem('s9')+'');
-        if (!!body) {
-        this.pxService.fichamedicaAuxFm(body).subscribe((response: any) => {
-          console.log("Paciente registrado con éxito, " + response.message);    
-        }, (error: any) =>{
-          console.log("Error al registrar paciente: " + error.error.error);
-        });  
-      }     
-         break;
+      // Continúa el resto de los casos...
     }
-  };
-  cleanSessionStorage(){
-    Object.keys(this.showSeccion).forEach(key => {
+  }
+
+  cleanSessionStorage() {
+    Object.keys(this.showSeccion).forEach((key) => {
       sessionStorage.removeItem(key);
     });
     sessionStorage.removeItem('currentSection');
